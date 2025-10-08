5b13885da59794db988e4d58868a69ae2b64460c
Lionel B <mauro.baldevenitez@mi.unc.edu.ar>
2025-10-08 11:14:53 -0300

    Completada semana 2

--- guia/semana2/src/main.cpp
+++ guia/semana2/src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include "readings.h"
+#include "../include/readings.h"
 
 using namespace std;
 
--- guia/semana2/src/readings.cpp
+++ guia/semana2/src/readings.cpp
@@ -1,4 +1,4 @@
-#include "readings.h"
+#include "../include/readings.h"
 #include <iostream>
 #include <fstream>
 #include <new>        // std::nothrow
@@ -13,16 +13,55 @@ float* crear_arreglo(int size) {
 
 void liberar_arreglo(float*& arr) {
     //TODO
+    delete[] arr;
+    arr = nullptr;
 }
-
+// Carga hasta 'size' valores desde archivo de texto en 'arr'.
+// Retorna la cantidad de valores leídos correctamente.
 int cargar_desde_archivo(const std::string& ruta, float* arr, int size) {
     //TODO
+    std::ifstream archivo;
+    archivo.open(ruta);
+
+    if(!archivo.is_open()){
+        std::cout<<"No se pudo abrir el archivo"<<std::endl;
+        return 0;
+    }
+    else{int leidos = 0;
+    float* p = arr;
+
+    while (leidos < size && (archivo >> *p)) {
+        p++;       // avanzar al siguiente float
+        leidos++;  // contar lectura
+    }
+
+    return leidos;}
 }
 
+
+
+// Guarda 'size' valores de 'arr' en archivo de texto (uno por línea).
+// Retorna true si se guardó con éxito, false si hubo error.
 bool guardar_en_archivo(const std::string& ruta, const float* arr, int size) {
     // TODO
+    std::ofstream archivo;
+    archivo.open(ruta);
+    if(!archivo.is_open()){
+        std::cout << "No se pudo abrir el archivo"<<std::endl;
+        return false;
+    }
+ for (int i = 0; i < size; i++) {
+        archivo << *(arr + i) << "\n";
+    }
+
+    return true; // <-- importante
+
 }
 
 void imprimir(const float* arr, int size) {
     // TODO
+    for(int i=0; i<size; i++){
+        std::cout << *(arr+i)<<std::endl;
+    }
+
 }
